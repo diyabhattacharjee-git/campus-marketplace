efe18657d4051b32e82309e47c7f454c180bd85c
Diya Bhattacharjee <dipabhattacharjee720@gmail.com>
2026-08-09 21:53:19 +0530

    Update

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '@/components/layout/Navbar';
 import Sidebar from '@/components/layout/Sidebar';
+import { useBiddingRealtime } from '@/hooks/useBiddingRealtime';
 
 export default function DashboardLayout() {
+  useBiddingRealtime();
+
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
